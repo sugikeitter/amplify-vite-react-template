b8ee99ab206110c1ec0fb59ad8ad6b34d8c16dd6
sugikeitter <s.l.n.f.13_kei.s@me.com>
2025-11-12 16:44:16 +0900

    Add AWS Amplify UI styles import
    Import AWS Amplify UI styles for better UI integration.

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { Authenticator } from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css';
+
 import App from "./App.tsx";
 import "./index.css";
 import { Amplify } from "aws-amplify";
